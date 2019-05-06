e97e35233387abdea53421ded1eb1a1b35dc9f17
Michael Vasquez <50000051+mvas8037@users.noreply.github.com>
2019-05-06 19:00:55 -0400

    Add files via upload

---
+++ config.hpp
@@ -0,0 +1,13 @@
+#include "main.h"
+
+#define CONFIG_H
+
+extern pros::Motor lfront ;
+extern pros::Motor lback ;
+extern pros::Motor rfront ;
+extern pros::Motor rback ;
+
+extern pros::Controller Controller1; // master control
+//extern pros::Controller Controller2; //partner control
+extern pros::ADIEncoder encoder;
+extern pros::ADIGyro gyro;
